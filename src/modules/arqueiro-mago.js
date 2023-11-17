271b418fbed4a944f414750ee2fad286411450e9
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-17 12:36:08 -0300

    Commit 5

--- src/modules/arqueiro-mago.js
+++ src/modules/arqueiro-mago.js
@@ -8,6 +8,29 @@ export class ArqueiroMago extends Personagem {
   static tipo = 'ArqueiroMago';
   static descricao ='Detentor de lancas e flechas mágicas!';
 
+  constructor(nome, destreza, elementoMagico, levelMagico, inteligencia) {
+    super(nome);
+    // Composição
+    this.ladoArqueiro = new Arqueiro(nome, destreza);
+    this.ladoMago = new Mago(nome, elementoMagico, levelMagico, inteligencia);
+  }
+
+  obterInsignia() {
+    return `${this.ladoArqueiro.obterInsignia()} = ${this.ladoMago.obterInsignia()}`;
+  }
+}
+
+/*
+import { Arqueiro } from "./arqueiro.js";
+import { Mago } from "./mago.js";
+import { Personagem } from "./personagem.js";
+
+export class ArqueiroMago extends Personagem {
+  ladoArqueiro;
+  ladoMago;
+  static tipo = 'ArqueiroMago';
+  static descricao ='Detentor de lancas e flechas mágicas!';
+
   constructor(nome, level, destreza, elementoMagico, levelMagico, inteligencia) {
     super(nome, level);
     // Composição
@@ -19,3 +42,4 @@ export class ArqueiroMago extends Personagem {
     return `${this.ladoArqueiro.obterInsignia()} = ${this.ladoMago.obterInsignia()}`;
   }
 }
+*/
---
+++ src/modules/guerreiro.js
@@ -0,0 +1,20 @@
+import { Personagem } from "./personagem.js";
+
+export class Guerreiro extends Personagem {
+  forca;
+  static tipo = "Guerreiro";
+  static descricao = 'Você será esmagado pela fúria do Guerreiro!';
+
+  constructor(nome, forca) {
+    super(nome);
+    this.forca = forca;
+  }
+
+  // Polimorfismo
+  obterInsignia() {
+    if (this.level >= 7 && this.forca >= 5) {
+      return `Guerreiro furioso`;
+    }
+    return super.obterInsignia();
+  }
+}
